82e33b0e432b695a58f3c8b3c29b565ad350daef
Kiss György <kissgyorgy@me.com>
2018-09-14 12:58:47 +0200

    Change /dirs endpoint url for better separate parameter
    Because it looks like an URL path, but it is a file system path, but I don't
    want to enforce url encoded paths (%2F) to make it simple.

--- linux_rest_api/__init__.py
+++ linux_rest_api/__init__.py
@@ -6,5 +6,5 @@ from .filesystem import dirs_api
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(system.bp, url_prefix="/system")
-    app.register_blueprint(dirs_api.bp, url_prefix="/dirs")
+    app.register_blueprint(dirs_api.bp, url_prefix="/dirs:")
     return app
